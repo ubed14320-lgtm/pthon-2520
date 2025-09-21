46eb53d415087dc40ff1938ac8396ff8971a49fd
ubed14320 <ubed14320@gmail.com>
2025-09-21 09:54:11 +0530

    basics_01

---
+++ python3/demo.py
@@ -0,0 +1,47 @@
+# means Comment, python will skip execution of this line
+# We used script mode to execute below code
+
+# Syntax (Rules)
+print(9) # 9 is numeric 
+# print(good morning) # SyntaxError: invalid syntax. Perhaps you forgot a comma?
+# good morning is text data 
+# in python rule is, if you have text data then we need to enclose that in "" or ''
+print("good morning")
+print('good morning')
+
+
+# print(hello) # NameError: name 'hello' is not defined. Did you mean: 'help'?
+hello = "hello hi"
+print(hello)
+
+print("hello")
+
+# name is identifier 
+
+# class = "10th Standard" # SyntaxError: invalid syntax --> class is a keyword
+classes = "10th Standard"
+print(classes)
+
+print("class")
+
+# Identifiers
+data = "hello good morning"
+print(data)
+# 2day_data = "hello" # invalid as starts with digit
+# print(2day_data)
+_data = "hello"
+print(_data)
+
+# $data = "hello" # SyntaxError: invalid syntax cannot have $ only _ allowed
+# print($data)
+
+# Improper way of using Identifiers
+x = "Ravi"
+y = 30
+z = "Python"
+
+
+# Proper way of using Identifiers
+student_name = "Ravi"
+student_name = 30
+student_enrolled_course = "python"
